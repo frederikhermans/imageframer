d1763859138ed7a3a5191f69cd56bbe9909707f6
Frederik Hermans <frederik.ac.hermans@gmail.com>
2016-04-12 12:41:59 +0200

    Fixed setup.py to properly install package.

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import setuptools
 
 setuptools.setup(name='imageframer',
+                 packages=setuptools.find_packages(),
                  version='0.0.0.dev1',
                  license='BSD')
